de42a36ddc2e7d1177e1bf2efa327e09f9ca34d5
Weston <29180454+westonnelson@users.noreply.github.com>
2025-06-16 21:28:37 -0500

    chore: configure next-sitemap with base URL and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/**
+ * Next.js Sitemap Configuration
+ * https://github.com/iamvishnusankar/next-sitemap
+ */
+module.exports = {
+  siteUrl: 'https://www.quotelinker.com',
+  generateRobotsTxt: true,
+  // optional: exclude certain pages
+  // exclude: ['/admin/*'],
+};
